4123c819cb8e63daded0454646f21d23b0402b33
Emil Strømsvåg <emilchri@hotmail.com>
2023-01-20 13:40:02 +0100

    Revert generate_explenations to TensorFlow 1.4

--- generate_explanations.py
+++ generate_explanations.py
@@ -1,5 +1,5 @@
 import os
-from tf_v2.flex import FLEX
+from tf_v1.flex import FLEX
 import tables
 import argparse
 # from nltk.tokenize.moses import MosesDetokenizer
@@ -24,7 +24,8 @@ def generate_explanations_for_one_image(image_name):
     vocab_size = data_dict['num_words']  # no. words in vocab
     id_to_word = data_dict['id_to_word']  # dict: integer id to english word
     word_to_id = data_dict['word_to_id']  # dict: english word to id
-    max_length = data_dict['max_length']  # max length of sentence, or X_sentence.shape[-1]
+    # max length of sentence, or X_sentence.shape[-1]
+    max_length = data_dict['max_length']
 
     with open(params.TEST_IMAGE_NAMES, 'rb') as f:
         test = [line.rstrip().decode('utf-8') for line in f.readlines()]
@@ -33,7 +34,8 @@ def generate_explanations_for_one_image(image_name):
     print('Done loading data_dict')
 
     # -------- LOAD MODEL --------
-    model_path = os.path.join(params.MODEL_SAVE_FOLDER, opt.model_version, 'flex')
+    model_path = os.path.join(params.MODEL_SAVE_FOLDER,
+                              opt.model_version, 'flex')
 
     model_params = {
         'img_embed_size': params.IMG_EMBED_SIZE,
@@ -59,12 +61,14 @@ def generate_explanations_for_one_image(image_name):
     index = test_names.index(image_name)
     feat = data_f.root.data[index, :]
     feat = np.expand_dims(feat, axis=0)
-    sentence, indices = flex_model.get_explanation(feat, id_to_word, word_to_id)
+    sentence, indices = flex_model.get_explanation(
+        feat, id_to_word, word_to_id)
     sent = [id_to_word.get(s) for s in indices]
     sent = detokenizer.detokenize(sent, return_str=True)
     print(test[index] + ": ", sent)
 
-    class_name = " ".join(i.capitalize() for i in image_name.lower().split('/')[-1].split("_")[:-2])
+    class_name = " ".join(i.capitalize()
+                          for i in image_name.lower().split('/')[-1].split("_")[:-2])
 
     exp = "This is a <b>" + class_name + "</b> because, <br>" + sent
     return exp
@@ -82,7 +86,8 @@ def generate_explanations():
     vocab_size = data_dict['num_words']  # no. words in vocab
     id_to_word = data_dict['id_to_word']  # dict: integer id to english word
     word_to_id = data_dict['word_to_id']  # dict: english word to id
-    max_length = data_dict['max_length']  # max length of sentence, or X_sentence.shape[-1]
+    # max length of sentence, or X_sentence.shape[-1]
+    max_length = data_dict['max_length']
     print('Done extracting objects from data_dict')
 
     with open(params.TEST_IMAGE_NAMES, 'rb') as f:
@@ -90,7 +95,8 @@ def generate_explanations():
         test_names = [str(f).split("/")[-1] for f in test]
 
     # -------- LOAD MODEL --------
-    model_path = os.path.join(params.MODEL_SAVE_FOLDER, opt.model_version, 'flex')
+    model_path = os.path.join(params.MODEL_SAVE_FOLDER,
+                              opt.model_version, 'flex')
 
     model_params = {
         'img_embed_size': params.IMG_EMBED_SIZE,
@@ -116,14 +122,16 @@ def generate_explanations():
         index = test_names.index(img_name)
         feat = data_f.root.data[index, :]
         feat = np.expand_dims(feat, axis=0)
-        sentence, indices = flex_model.get_explanation(feat, id_to_word, word_to_id)
+        sentence, indices = flex_model.get_explanation(
+            feat, id_to_word, word_to_id)
         sent = [id_to_word.get(s) for s in indices]
         sent = detokenizer.detokenize(sent, return_str=True)
         explanation = test_names[index] + ": " + sent
         print(str(i)+':'+explanation)
         explanations.append(explanation)
 
-    target = open(os.path.join(params.MODEL_SAVE_FOLDER, opt.model_version, 'explanations_' + opt.model_version+".txt"), 'w')
+    target = open(os.path.join(params.MODEL_SAVE_FOLDER, opt.model_version,
+                  'explanations_' + opt.model_version+".txt"), 'w')
     target.write('\n'.join(explanations))
     target.close()
 
@@ -132,9 +140,12 @@ def generate_explanations():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--method', default='generate_exp', help='Which method you want to execute')
-    parser.add_argument('--model_version', default='flex_v1', help='Name of the trained model')
-    parser.add_argument('--image_name', default='Cerulean_Warbler_0005_797206,jpg', help='The test image for which you want to generate the explanation')
+    parser.add_argument('--method', default='generate_exp',
+                        help='Which method you want to execute')
+    parser.add_argument('--model_version', default='flex_v1',
+                        help='Name of the trained model')
+    parser.add_argument('--image_name', default='Grasshopper_Sparrow_0078_116052.jpg',
+                        help='The test image for which you want to generate the explanation')
 
     opt = parser.parse_args()
 
@@ -142,5 +153,3 @@ if __name__ == '__main__':
         generate_explanations()
     elif opt.method == 'generate_one_exp':
         generate_explanations_for_one_image(opt.image_name)
-
-
